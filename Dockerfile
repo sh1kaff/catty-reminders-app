eb6d7f6da6efb4925d7ebb5e87e74cd80304b782
sh1kaff <63544208+sh1kaff@users.noreply.github.com>
2026-04-12 23:01:12 +0400

    lab3

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.12
+
+WORKDIR /catty-reminders-app
+
+COPY requirements.txt .
+RUN pip install -r requirements.txt
+
+COPY app/ ./app/
+COPY static/ ./static/
+COPY templates/ ./templates/
+COPY config.json .
+
+EXPOSE 8181
+
+CMD["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8181"]
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,51 +7,54 @@ if [ -z "$DEPLOY_HOST" ] || [ -z "$DEPLOY_USER" ]; then
     exit 1
 fi
 
-if [ -z "$RELEASE_HASH" ]; then
-    echo "Error: RELEASE_HASH must be set"
+if [ -z "$RELEASE_HASH" ] || [ -z "$IMAGE_NAME" ]; then
+    echo "Error: RELEASE_HASH and IMAGE_NAME must be set"
     exit 1
 fi
 
 DEPLOY_PORT=${DEPLOY_PORT:-22}
-DEPLOY_DIR="/mnt/c/Users/Sergo/Documents/prog/university/catty-reminders-app"
+CONTAINER_NAME="catty-app"
+PORT="8181"
+IMAGE="${IMAGE_NAME,,}:$RELEASE_HASH"
 
 echo "Deploying to $DEPLOY_HOST:$DEPLOY_PORT"
 echo "User: $DEPLOY_USER"
-#echo "Release hash: $RELEASE_HASH"
-echo "Release branch: $RELEASE_BRANCH"
+echo "Release hash: $RELEASE_HASH"
+echo "Image: $IMAGE"
 
 SSH_OPTIONS="-p $DEPLOY_PORT -o StrictHostKeyChecking=no"
 
 ssh $SSH_OPTIONS "$DEPLOY_USER@$DEPLOY_HOST" << EOF
     set -e
     
-    cd $DEPLOY_DIR
+    echo "Logging in to GitHub Container Registry..."
+
+    echo "$DOCKER_TOKEN" | docker login ghcr.io -u "$GITHUB_ACTOR" --password-stdin
+
+    echo "Pulling image: \$IMAGE"
+    docker pull \$IMAGE
     
-    git fetch origin
-    git checkout $RELEASE_HASH
+    echo "Stopping old container..."
+
+    docker stop $CONTAINER_NAME || true
+    docker rm $CONTAINER_NAME || true
     
-    DEPLOY_REF=\$(git rev-parse HEAD)
-    echo "DEPLOY_REF=\$DEPLOY_REF" > .env.deploy
-    echo "Deployed version: \$DEPLOY_REF"
-    
-    if [ ! -d ".venv" ]; then
-        python3 -m venv .venv/
-    fi
-    
-    source .venv/bin/activate
-    
-    if [ -f "requirements.txt" ]; then
-        pip install -r requirements.txt
-    fi
-    
-    sudo systemctl restart app.service
+    echo "Starting new container..."
+
+    docker run -d \
+        -p $PORT:$PORT \
+        --name $CONTAINER_NAME \
+        --restart unless-stopped \
+        -e DEPLOY_REF=$RELEASE_HASH \
+        \$IMAGE
     
     sleep 4
     
-    if sudo systemctl is-active --quiet app.service; then
+    if docker ps | grep -q $CONTAINER_NAME; then
         echo "Deployment completed successfully"
     else
         echo "ERROR: Application failed to start"
+        docker logs $CONTAINER_NAME
         exit 1
     fi
 EOF
